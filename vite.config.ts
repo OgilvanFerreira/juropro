15ed062a9edbbfcd41d5d9f7e0f23cbfcb5e3400
Gilvan Ferreira <38859380+OgilvanFerreira@users.noreply.github.com>
2026-04-29 08:36:39 -0300

    Refactor vite.config.ts for improved imports and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { nitro } from 'nitro/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     nitro({
-      preset: 'vercel', // ← ESTE é o ponto crítico
+      preset: "vercel",
     }),
-    viteReact(),
+    react(),
+    tsconfigPaths(),
   ],
-})
+});
